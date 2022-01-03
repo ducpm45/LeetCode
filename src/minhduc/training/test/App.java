e0091b4425f12ac2334951cf8627c3bf349ecf98
MinhDuc0602 <pminhduc62110@gmail.com>
2022-01-03 20:13:17 +0700

    2022/1/3

--- src/minhduc/training/test/App.java
+++ src/minhduc/training/test/App.java
@@ -1,16 +1,24 @@
 package minhduc.training.test;
 
-import java.util.Arrays;
-
-import minhduc.training.leetcode._912_Sort_an_Array;
+import minhduc.training.leetcode._21_Merge_Two_Sorted_Lists;
 
 
 public class App {
 	public static void main(String[] args) {
-		_912_Sort_an_Array test = new _912_Sort_an_Array();
-		int[] a = { 5, 7, 11, 3, 9, 2, 6 };
-		test.sortArray(a);
-		System.out.println(Arrays.toString(test.sortArray(a)));
+		_21_Merge_Two_Sorted_Lists test = new _21_Merge_Two_Sorted_Lists();
+		ListNode list1 = new ListNode(1);
+		ListNode n1 = new ListNode(2);
+		ListNode n2 = new ListNode(4);
+		list1.next = n1;
+		n1.next = n2;
+
+		ListNode list2 = new ListNode(1, null);
+		ListNode n3 = new ListNode(3);
+		ListNode n4 = new ListNode(4);
+		list2.next = n3;
+		n3.next = n4;
+
+		test.mergeTwoLists(list1, list2);
 	}
 
 }
---
+++ src/minhduc/training/test/ListNode.java
@@ -0,0 +1,19 @@
+package minhduc.training.test;
+
+public class ListNode {
+	public int val;
+	public ListNode next;
+
+	ListNode() {
+
+	}
+
+	public ListNode(int val) {
+		this.val = val;
+	}
+
+	public ListNode(int val, ListNode next) {
+		this.val = val;
+		this.next = next;
+	}
+}
